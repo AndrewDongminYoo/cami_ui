745c07b34b52687d61d54ff493b79ca19e99284d
Dongmin Yu <ydm2790@gmail.com>
2023-11-09 02:09:10 +0900

    feat: 🌐 로컬라이제이션 설정 파일 추가

---
+++ lib/l10n/app_localizations_ko.dart
@@ -0,0 +1,10 @@
+// 🌎 Project imports:
+import 'app_localizations.dart';
+
+/// The translations for Korean (`ko`).
+class LocalizedKo extends Localized {
+  LocalizedKo([String locale = 'ko']) : super(locale);
+
+  @override
+  String get acceptAll => '카미랩';
+}
