8df94420d4803505eb735efd2a87fce4185c46d5
Dongmin Yu <ydm2790@gmail.com>
2023-11-09 05:19:13 +0900

    chore: 🍱 color palette 선언

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,148 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  AppColors._();
+
+  /// Color: #000000
+  static const Color black900 = Color(0xFF000000);
+
+  /// Color: #E6F0FF
+  static const Color blue50 = Color(0xFFE6F0FF);
+
+  /// Color: #1D59C4
+  static const Color blue800 = Color(0xFF1D59C4);
+
+  /// Color: #D1D5DB
+  static const Color blueGray100 = Color(0xFFD1D5DB);
+
+  /// Color: #D4D4D4
+  static const Color blueGray10001 = Color(0xFFD4D4D4);
+
+  /// Color: #6BCCCCCC
+  static const Color blueGray1006b = Color(0x6BCCCCCC);
+
+  /// Color: #9CA3AF
+  static const Color blueGray300 = Color(0xFF9CA3AF);
+
+  /// Color: #4B5563
+  static const Color blueGray700 = Color(0xFF4B5563);
+
+  /// Color: #525252
+  static const Color blueGray70001 = Color(0xFF525252);
+
+  /// Color: #374151
+  static const Color blueGray800 = Color(0xFF374151);
+
+  /// Color: #1F2937
+  static const Color blueGray900 = Color(0xFF1F2937);
+
+  /// Color: #F0803D
+  static const Color deepOrange400 = Color(0xFFF0803D);
+
+  /// Color: #F0803D
+  static const Color errorContainer = Color(0xFFF0803D);
+
+  /// Color: #888888
+  static const Color gray = Color(0xFF888888);
+
+  /// Color: #F3F4F6
+  static const Color gray100 = Color(0xFFF3F4F6);
+
+  /// Color: #F5F5F5
+  static const Color gray10001 = Color(0xFFF5F5F5);
+
+  /// Color: #E5E7EB
+  static const Color gray200 = Color(0xFFE5E7EB);
+
+  /// Color: #EEEEEE
+  static const Color gray20001 = Color(0xFFEEEEEE);
+
+  /// Color: #E5E5E5
+  static const Color gray300 = Color(0xFFE5E5E5);
+
+  /// Color: #FAFAFA
+  static const Color gray50 = Color(0xFFFAFAFA);
+
+  /// Color: #A3A3A3
+  static const Color gray500 = Color(0xFFA3A3A3);
+
+  /// Color: #4C939393
+  static const Color gray5004c = Color(0x4C939393);
+
+  /// Color: #757575
+  static const Color gray600 = Color(0xFF757575);
+
+  /// Color: #6B7280
+  static const Color gray60001 = Color(0xFF6B7280);
+
+  /// Color: #5F6061
+  static const Color gray700 = Color(0xFF5F6061);
+
+  /// Color: #404040
+  static const Color gray800 = Color(0xFF404040);
+
+  /// Color: #202020
+  static const Color gray900 = Color(0xFF202020);
+
+  /// Color: #121212
+  static const Color gray90001 = Color(0xFF121212);
+
+  /// Color: #1F2020
+  static const Color gray90002 = Color(0xFF1F2020);
+
+  /// Color: #262626
+  static const Color gray90003 = Color(0xFF262626);
+
+  /// Color: #6FD574
+  static const Color green300 = Color(0xFF6FD574);
+
+  /// Color: #9BB1EB
+  static const Color indigo200 = Color(0xFF9BB1EB);
+
+  /// Color: #5079D7
+  static const Color indigo400 = Color(0xFF5079D7);
+
+  /// Color: #E5E5E5
+  static const Color onError = Color(0xFFE5E5E5);
+
+  /// Color: #171717
+  static const Color onErrorContainer = Color(0xFF171717);
+
+  /// Color: #1F2937
+  static const Color onPrimary = Color(0xFF1F2937);
+
+  /// Color: #FFFFFF
+  static const Color onPrimaryContainer = Color(0xFFFFFFFF);
+
+  /// Color: #262626
+  static const Color onSecondaryContainer = Color(0xFF262626);
+
+  /// Color: #FFFF50
+  static const Color primary = Color(0xFFFFFF50);
+
+  /// Color: #414142
+  static const Color primaryContainer = Color(0xFF414142);
+
+  /// Color: #D9D9D9
+  static const Color secondaryContainer = Color(0xFFD9D9D9);
+
+  /// Color: #FFFFFF
+  static const Color whiteA700 = Color(0xFFFFFFFF);
+
+  /// Color: #FEFFBF
+  static const Color yellow100 = Color(0xFFFEFFBF);
+
+  /// Color: #FFFF50
+  static const Color yellow400 = Color(0xFFFFFF50);
+}
