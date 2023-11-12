5b3c921fb68f8fbcd481086e85cf481bb5c8e581
Dongmin Yu <ydm2790@gmail.com>
2023-11-13 04:08:40 +0900

    perf: 🗃️ pet_breeds model 클래스 수정

--- lib/ui/new_cat_page/data/pet_breeds.model.dart
+++ lib/ui/new_cat_page/data/pet_breeds.model.dart
@@ -1,12 +1,13 @@
 // 🎯 Dart imports:
 import 'dart:convert';
 
-List<PetBreeds> catBreedsFromJson(String str) {
+List<PetBreeds> petBreedsFromJson(String str) {
   final jsonData = json.decode(str);
-  return List<PetBreeds>.from(jsonData.map(PetBreeds.fromJson));
+  return List<PetBreeds>.from(jsonData
+      .map((dynamic data) => PetBreeds.fromJson(data as Map<String, dynamic>)));
 }
 
-String catBreedsToJson(List<PetBreeds> data) {
+String petBreedsToJson(List<PetBreeds> data) {
   final dyn = List<dynamic>.from(data.map((x) => x.toJson()));
   return json.encode(dyn);
 }
@@ -23,19 +24,37 @@ class PetBreeds {
         animalGroup1Id: json['animal_group1_id'],
         animalGroup2Id: json['animal_group2_id'],
         animalGroup2Name: json['animal_group2_name'],
-        breedSizeName: json['breed_size_name'],
+        breedSizeName: breedSizeValues.map[json['breed_size_name']]!,
       );
-
-  /// 1: 멍멍, 2: 냥냥
   int animalGroup1Id;
   int animalGroup2Id;
   String animalGroup2Name;
-  String breedSizeName;
+  BreedSizeName breedSizeName;
 
   Map<String, dynamic> toJson() => {
         'animal_group1_id': animalGroup1Id,
         'animal_group2_id': animalGroup2Id,
         'animal_group2_name': animalGroup2Name,
-        'breed_size_name': breedSizeName,
+        'breed_size_name': breedSizeValues.reverse[breedSizeName],
       };
+}
+
+enum BreedSizeName { EMPTY, MEDIUM, LARGE, SMALL }
+
+final breedSizeValues = EnumValues({
+  '': BreedSizeName.EMPTY,
+  '중형': BreedSizeName.MEDIUM,
+  '소형': BreedSizeName.SMALL,
+  '대형': BreedSizeName.LARGE
+});
+
+class EnumValues<T> {
+  EnumValues(this.map);
+  Map<String, T> map;
+  Map<T, String>? reverseMap;
+
+  Map<T, String> get reverse {
+    reverseMap ??= map.map((k, v) => MapEntry(v, k));
+    return reverseMap!;
+  }
 }
