57d72253998ca3d64a65e17c0a623ee427df0d6e
Dongmin Yu <ydm2790@gmail.com>
2023-11-14 21:32:42 +0900

    style: 💄 Implement a wallpaper layout that only appears in wide viewports

---
+++ lib/layout/desktop_layout.dart
@@ -0,0 +1,73 @@
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// 🌎 Project imports:
+import '/core/utils/size_utils.dart';
+import '/gen/assets.gen.dart';
+import '/theme/theme_helper.dart';
+import '/widgets/custom_image_view.dart';
+
+class DesktopLayout extends StatelessWidget {
+  const DesktopLayout({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, window) {
+        final isWide = window.maxWidth >= 425.0;
+        final main = isWide ? FIGMA_WIDTH : window.maxWidth;
+        final sub = isWide ? window.maxWidth - FIGMA_WIDTH : window.maxWidth;
+        final height = window.maxHeight;
+        return Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Visibility(
+              visible: isWide,
+              child: Scaffold(
+                extendBody: true,
+                extendBodyBehindAppBar: true,
+                body: Container(
+                  width: window.maxWidth,
+                  height: height,
+                  color: lightTheme.primary,
+                  child: CustomImageView(
+                    alignment: Alignment.bottomCenter,
+                    imagePath: Assets.images.imgMainBackground.path,
+                    width: window.maxWidth * 0.71,
+                    height: window.maxWidth * 0.29,
+                  ),
+                ),
+              ),
+            ),
+            Builder(
+              builder: (context) {
+                ScreenUtil.configure(
+                  data: MediaQueryData(size: Size(main, height)),
+                  designSize: figmaFrameSize,
+                  minTextAdapt: true,
+                  splitScreenMode: true,
+                );
+                ScreenUtil.registerToBuild(context, true);
+                return Padding(
+                  padding: EdgeInsets.only(left: sub * 0.7, right: sub * 0.3),
+                  child: SizedBox(
+                    width: main,
+                    child: child,
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
